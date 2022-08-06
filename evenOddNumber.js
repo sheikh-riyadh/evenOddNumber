6acd0f31813ba1abbb29033a66e29a07b580a65c
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 20:30:28 +0600

    we finding number is even or odd

---
+++ evenOddNumber.js
@@ -0,0 +1,11 @@
+function odd_even(number) {
+    if (number % 2 == 0) {
+        console.log("The number is even", number);
+    }
+    else {
+        console.log("The number is odd", number);
+    }
+}
+
+
+odd_even(11);
